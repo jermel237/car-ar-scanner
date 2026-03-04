99bab30caf6cc657cef932cd87d8ceb751c62acd
Jermel S Hernandez <jermelxbishi2@gmail.com>
2026-03-04 15:36:16 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4465,7 +4465,7 @@ function buildSceneContent(
         const isHl = highlightIndex === i;
         const isFront = i === 0;
         let extraX = 0;
-        let carScale = 2;
+        let carScale = 1.5;
         let shouldRender = true;
         let carOpacity = 1;
         let carScaleEffect = 1;
